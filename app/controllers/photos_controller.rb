852f2699f7a63ab0fe624461aa8462f97450d62f
Thomas Mitchell <tmitche831@gmail.com>
2014-10-01 17:13:48 +1000

    controller redo, index imagetags

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,21 +2,20 @@ class PhotosController < ApplicationController
 	require 'flickraw'
 
 	def index
-		render
+		# https://github.com/hanklords/flickraw
+		FlickRaw.api_key="0436770df28381222f1f8b15aa3dcda8"
+		FlickRaw.shared_secret="8854eb4bb81d798f"
+		@photos = flickr.interestingness.getList( :per_page => 5 )
+
+		@photos.each do |pic|
+			photos_info = flickr.photos.getInfo(:photo_id => pic.id)
+			photo_url = FlickRaw.url_m(photos_info)
+			puts "You can find this photo at #{photo_url}"
+		end
 	end
 
 	def show
-		FlickRaw.api_key = "0436770df28381222f1f8b15aa3dcda8"
-		FlickRaw.shared_secret = "8854eb4bb81d798f"
 
-		url = params[:url]
-		info = flickr.photos.getInfo(:photo_id => url.split("/").last)
-		@embed_photo = {}
-		@embed_photo["flickr"]=FlickRaw.url(info) rescue FlickRaw.url_o(info) rescue FlickRaw.url_b(info)
-		@title = info.title
-		@square_url = FlickRaw.url_s(info)
-		@taken = info.dates.taken
-		@views = info.views
-		@tags = info.tags.map{|t| t.raw}
+	
 	end
 end
