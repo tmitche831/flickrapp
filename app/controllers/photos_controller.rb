87dae9ae3dccb93dd5f7e5c4e4c1543a6b9c8255
Thomas Mitchell <tmitche831@gmail.com>
2014-10-02 14:34:06 +1000

    update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,16 @@ class PhotosController < ApplicationController
 
 	def index
 		# https://github.com/hanklords/flickraw
-		FlickRaw.api_key="0436770df28381222f1f8b15aa3dcda8"
-		FlickRaw.shared_secret="8854eb4bb81d798f"
+		FlickRaw.api_key="ac2666e61447397277d2cc9e75652f7b"
+		FlickRaw.shared_secret="22a4cfc651335bd8"
 		@photos = flickr.interestingness.getList( :per_page => 5 )
 
 		@photos.each do |pic|
 			photos_info = flickr.photos.getInfo(:photo_id => pic.id)
 			photo_url = FlickRaw.url_m(photos_info)
 			puts "You can find this photo at #{photo_url}"
+			puts "---------------------------------------"
+			puts "#{photos_info}"
 		end
 	end
 
